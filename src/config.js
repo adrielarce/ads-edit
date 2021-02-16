f8cd1668f77a3127c61379cb7323694dac6720f9
root <root@DESKTOP-O8ESL65.localdomain>
2021-02-16 16:32:08 -0500

    added stage variables for api

--- src/config.js
+++ src/config.js
@@ -1,7 +1,20 @@
-const config = {
+const prod = {
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://dlmail3s7j.execute-api.us-east-2.amazonaws.com/prod/",
+    },
+};
+const prod = {
     apiGateway: {
         REGION: "us-east-2",
         URL: "https://rtnkme5phc.execute-api.us-east-2.amazonaws.com/prod",
     },
 };
+const config = {
+    // Default to dev if not set
+    ...(process.env.REACT_APP_STAGE === "prod" ? prod : dev),
+};
+
+
+
 export default config;
